2060bab8bc1ffeceffbab0cf2a7acb3948ca7acc
Sameh Farouk <35781234+SAME74@users.noreply.github.com>
2018-12-04 14:37:36 +0200

    Add files via upload

--- Timer0/main.c
+++ Timer0/main.c
@@ -4,38 +4,44 @@
  * Created: 12/3/2018 3:03:27 PM
  * Author : Sameh Farouk
  */ 
-#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "timer.h"
 
-
-void fun(void){
-	
-	PORTA=0x01 ;
-	_delay_ms(100);
-	PORTA=0x00;
-}
-
-
+void fun(void);
 int main(void)
 {
-	DDRA=0xff;
-	TIMER_CONFG f ;
-	f.TIMER_MODE=NORMAL ;
-	f.PRESCALERS=PRESCALER_1024 ;
-	TIMER_INTERRUPT_CHECK d ;
-	init_timer0(0x20,&f);
-	d.fun_ptr=&fun ;
-	d.INTERRUPT_ENABLE=OVERVLOW_INTERRUPT ;
-	interrupt_timers(&d);
-    sei();
-    while (1) 
-    {
-		
+	DDRB=0xff; //Flashing LED
+TIMER_INTERRUPT_CHECK f;
 
+	//Setup Timer: use overflow interrupt.
+interrupt_timers(&f);
 
-    }
+	 write_to_count_reg(200);//15624
+sei();
+TIMER_CONFG s;
+s.TIMER_MODE=NORMAL ;
+s.PRESCALERS=NO_PRESCALER;
+init_timer0(&s);
+f.INTERRUPT_ENABLE=OVERVLOW_INTERRUPT;
+f.fun_ptr=&fun;
+	 // Enable global interrupts??
+	
+	//TCCR1B |= ((1 << CS10) | (1 << CS11)); // Set up timer at Fcpu/64 ??
+
+	PORTB=0x00;
+	
+	while (1)
+	{
+	}//end while
+}//end main
+
+void fun(void)
+{	//Timer is on:
+		PORTB=0x01;
+		_delay_ms(100);
+		PORTB=0x00;
+		_delay_ms(100); //turn on LED
+	write_to_count_reg(200) ;
 }
-
